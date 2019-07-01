3982d2e7d4060431b26a29648fff952c821b171b
TovarishFin <tovarishfin@gmail.com>
2019-07-01 21:18:01 +0200

    deployments for staging and prod

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 const publicPath = process.env.NODE_ENV === 'production' ? '/' : '/'
+const prodBucket = process.env.VUE_APP_S3D_BUCKET_PROD
+const stagingBucket = process.env.VUE_APP_S3D_BUCKET_STAGING
+const prodCloudfrontId = process.env.VUE_APPS3D_CLOUDFRONT_ID_PROD
+const stagingCloudfrontId = process.env.VUE_APPS3D_CLOUDFRONT_ID_STAGING
+
+const bucket =
+  process.env.NODE_ENV === 'production' ? prodBucket : stagingBucket
+
+const cloudfrontId =
+  process.env.NODE_ENV === 'production' ? prodCloudfrontId : stagingCloudfrontId
 
 module.exports = {
   configureWebpack: {
@@ -13,7 +23,7 @@ module.exports = {
       registry: undefined,
       awsProfile: 'default',
       region: 'eu-central-1',
-      bucket: 'crypto-weddings-dapp',
+      bucket,
       createBucket: true,
       staticHosting: true,
       staticIndexPage: 'index.html',
@@ -23,10 +33,10 @@ module.exports = {
       deployPath: '/',
       acl: 'public-read',
       pwa: false,
-      enableCloudfront: true,
+      //enableCloudfront: true,
       uploadConcurrency: 5,
       pluginVersion: '3.0.0',
-      cloudfrontId: 'E15IUUCLDQ4JRZ',
+      cloudfrontId,
       cloudfrontMatchers: '/*'
     }
   }
